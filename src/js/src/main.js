88cd3897fa7afaecce3b510981daeb959ba0c7b0
jonathan garrido <jonathan.garrido1988@gmail.com>
2016-09-16 21:19:39 -0300

    agrege txt y efecto modal

--- src/js/src/main.js
+++ src/js/src/main.js
@@ -32,3 +32,55 @@ var nav =  (function(){
   };
 
 })();
+
+var modal = (function(){
+
+  var settings = {
+        $modal: $('.modal'),
+        $conte: $('.modal__article'),
+        $modalmulti: $('.modal__multi'),
+        $modalconte: $('.modal__conte'),
+      },
+      
+      base = function (modal){
+        $('html').toggleClass( 'overflow'); 
+        $(modal).fadeToggle();
+        settings.$conte.toggleClass('modal--in');
+        settings.$conte.scrollTop(0);
+        console.log('asdasd')
+      },
+
+      close = function(){
+        $('html').toggleClass( 'overflow'); 
+        settings.$modal.fadeOut();
+        settings.$conte.toggleClass('modal--in');
+        settings.$conte.scrollTop(0);
+      },
+
+      multi = function(){
+        
+        base('.modal__multi');
+        settings.$modalmulti.find('.modal'+id).fadeIn(); 
+        if(!id){
+          settings.$modalmulti.find('.modal__article > div').fadeOut(); 
+        }
+      },
+
+      conte = function(event){
+        base('.modal__conte');
+        var $padre= $(event).parents(':eq(1)'),
+            $color= $padre.css('background-color');
+        $('.modal__conte > div').css('background-color','#009de0') ;
+        settings.$modalconte.find('.modal__article').css('background-color','transparent');
+        
+        var $txt = $padre.html();
+        settings.$modalconte.find('.modal--contenido').html($txt);
+      }
+
+  ;return{
+    base: base, 
+    multi: multi,
+    conte: conte,
+    close: close, 
+  }
+})();
