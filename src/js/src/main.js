6ed9085ee7990191d3936ab82eca4ea8d6f7448f
jonathan garrido <jonathan.garrido1988@gmail.com>
2016-09-20 23:01:57 -0300

    limpiar funciones no usadas

--- src/js/src/main.js
+++ src/js/src/main.js
@@ -1,23 +1,5 @@
 'use strict';
 
-var nav =  (function(){
-
-  var settings = {
-
-      },
-
-      full = function(){
-        $('html').toggleClass( 'overflow'); 
-        $( '.nav__full--barra' ).toggleClass( 'fadeInright');
-        $( '.nav__icon' ).toggleClass( 'icon-close icon-navicon');
-      }
-
-  ;return{
-    full : full,
-  };
-
-})();
-
 var modal = (function(){
 
   var settings = {
